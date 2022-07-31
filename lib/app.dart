e57172a464bf0e9a62634c65d81ae9296d945f4e
Arthur Abrantes <abrantesarthur1997@gmail.com>
2022-07-31 12:50:52 -0700

    create home.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'home.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Hello");
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'OpenSans',
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(title: 'EDEMA score home page'),
+    );
   }
 }
