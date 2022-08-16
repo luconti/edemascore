badd79686f46d76d24c4c3ad215d788c0e38e0a7
Arthur Abrantes <abrantesarthur1997@gmail.com>
2022-08-16 01:52:16 -0700

    populate dropDown content

--- lib/widgets/custom_dropdown.dart
+++ lib/widgets/custom_dropdown.dart
@@ -10,7 +10,7 @@ class DropDownInput {
   DropDownInput({required this.buttonTitle, required this.content});
 }
 
-// TODO: update content for other fields
+// TODO: ask Lucas for links to studies
 List<DropDownInput> dropdownInputs = [
   DropDownInput(
     buttonTitle: "When to Use",
@@ -19,12 +19,23 @@ List<DropDownInput> dropdownInputs = [
   ),
   DropDownInput(
     buttonTitle: "Pearls/Pitfalls",
-    content: "second content",
+    content: "Pearls:\n\n"
+        "    • To our knowledge, the EDEMA score was developed from the largest single cohort of patients with malignant edema in the literature at the time.\n"
+        "    • The EDEMA score has been externally validated in a cohort of 478 patients in Cheng et al, 2020.\n"
+        "    • Better identification of patients with moderate to severe risk of developing LTME can inform clinical management such as planning decompressive hemicraniectomies, transfer to tertiary care centers, and improved family discussions.\n"
+        "    • Increasing EDEMA scores (0-14) indicate a higher probability of LTME.\n"
+        "    • In patients who received an EDEMA score of ≥ 7, the positive predictive value of LTME was 93%.\n\n"
+        "Pitfalls:\n\n"
+        "    • This tool is meant to help clinicians stratify a patient’s risk of LTME but does not reflect all or other factors that may influence clinical decision making.\n"
+        "    • A modified EDEMA score that includes the patient’s NIHSS on admission improved discrimination\n"
+        "    • Advanced imaging was not included in the EDEMA score because MRI may not be feasible at all centers. The score relies on data readily available to most practitioners within the first 24 hours of stroke.\n"
+        "    • Presumably not all of the patients in the study who died would have survived with decompressive hemicraniectomy, and therefore our score only identifies those who develop LTME, not those who would benefit from surgery.\n",
   ),
   DropDownInput(
-    buttonTitle: "Why Use",
-    content: "Third content",
-  ),
+      buttonTitle: "Why Use",
+      content:
+          "    Each year, approximately 10% of the ~800,000 Americans hospitalized for ischemic stroke develop clinically significant brain swelling and mass effect within the first week. The risk can be 30-40% in patients with middle cerebral artery (MCA) occlusion.\n\n"
+          "    Rapid recognition of patients who will develop LTME after large hemispheric infarction is essential for appropriate triage to tertiary care centers and possible decompressive hemicraniectomy (DHC)."),
 ];
 
 class CustomDropdown extends StatefulWidget {
